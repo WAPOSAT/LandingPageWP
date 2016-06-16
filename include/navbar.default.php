5096cb0c7e2a44808d0034fcdf7e7d24ee7b177a
JOTAI <juan.basflo.initec@gmail.com>
2016-06-16 11:28:30 -0500

    ultimos cambios de botones head

--- include/navbar.default.php
+++ include/navbar.default.php
@@ -7,6 +7,18 @@
     }
 </style>
 
+<!-- Analytics Waposat -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-79440686-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin:0px">
     <div class="container">
         <div class="navbar-header">
@@ -26,9 +38,9 @@
                 <li class="dropdown">
                     <a href="#" class="droptown-toggle" data-toggle="dropdown">Mediciones Tiempo Real<b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="EstamosTrabajando.php">Estacion RioRimac</a></li>
-                        <li class="divider"></li>
                         <li><a href="monitoreo.php">CITRAR_UNI</a></li>
+                        <li class="divider"></li>
+                        <li><a href="EstamosTrabajando.php">Estacion RioRimac</a></li>
                     </ul>
                 </li>
             </ul>
